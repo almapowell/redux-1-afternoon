c4dbd9b079738cdc731257db17fe17cff3c6e4b8
Alma Powell <alma111000@gmail.comgit config --global user.email alma111000@gmail.comgit config --global user.name Alma>
2019-07-10 19:04:29 -0600

    commit

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -14,17 +14,6 @@ class Author extends Component {
   }
 
 
-  handleAuthorFirstChange(nameVal) {
-    this.setState({
-      authorFirst: nameVal
-    });
-  }
-
-  handleAuthorLastChange(nameVal) {
-    this.setState({
-      authorLast: nameVal
-    });
-  }
   saveChanges() {
     store.dispatch({
       type: UPDATE_AUTHOR_FIRST,
@@ -36,36 +25,6 @@ class Author extends Component {
     });
   }
 
-  render() {
-    return (
-      <div className="Author forms">
-        <div className="input_container">
-          <h2>Author First Name:</h2>
-          <input
-            value={this.state.authorFirst}
-            onChange={e => this.handleAuthorFirstChange(e.target.value)}
-          />
-        </div>
-        <div className="input_container">
-          <h2>Author Last Name:</h2>
-          <input
-            value={this.state.authorLast}
-            onChange={e => this.handleAuthorLastChange(e.target.value)}
-          />
-        </div>
-        <Link to="/add/name">
-          <button onClick={() => this.saveChanges()} className="left_button">
-            Previous
-          </button>
-        </Link>
-        <Link to="/add/ingredients">
-          <button onClick={() => this.saveChanges()} className="right_button">
-            Next
-          </button>
-        </Link>
-      </div>
-    );
-  }
 }
 
 export default Author;
